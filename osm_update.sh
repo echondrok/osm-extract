924e938ab4bbe4d17930a9bcd6d71a693106b08d
echondrok <echondrok@gmail.com>
2017-12-01 17:18:22 +0200

    Updates mirror link and database store for the Risk Data Portal of Thessaloniki.

--- osm_update.sh
+++ osm_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:/opt/osmosis/bin
-cd /home/ubuntu/osm-extract
+export PATH=$PATH:/root/osmosis/bin
+cd /root/osm-extract
 echo $(date)
-make all NAME=malawi URL="http://download.openstreetmap.fr/extracts/africa/malawi-latest.osm.pbf" && psql -d geonode -f set_pub_date.sql
+make all NAME=thessaloniki URL="https://s3.amazonaws.com/metro-extracts.mapzen.com/thessaloniki_greece.osm.pbf" && psql -d geonode -f set_pub_date.sql
 echo $(date)
