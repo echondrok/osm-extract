a2dc94c0689f64012a3d334def89c302b7ccb9cb
Evangelos Chondrokostas <echondrok@gmail.com>
2017-12-05 21:34:30 +0200

    Update osm_update.sh

--- osm_update.sh
+++ osm_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:/root/osmosis/bin
-cd /root/osm-extract
+export PATH=$PATH:/home/root/osmosis/bin
+cd /home/root/osm-extract
 echo $(date)
-make all NAME=thessaloniki URL="https://s3.amazonaws.com/metro-extracts.mapzen.com/thessaloniki_greece.osm.pbf" && psql -d thess_geonode -f set_pub_date.sql
+make all NAME=thessaloniki URL="https://s3.amazonaws.com/metro-extracts.mapzen.com/thessaloniki_greece.osm.pbf" && psql -d thess_geonode_app -f set_pub_date.sql
 echo $(date)
